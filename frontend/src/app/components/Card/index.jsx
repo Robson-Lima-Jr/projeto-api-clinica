1477e2ce26e1257f6a72160221326dba7d24ad34
Rob Jr <robsonjuniorsn@gmail.com>
2025-11-19 17:28:06 -0300

    area principal e cards com dados da api adicionados

--- frontend/src/app/components/Card/index.jsx
+++ frontend/src/app/components/Card/index.jsx
@@ -1,19 +1,28 @@
 import styles from './Card.module.css';
 
-export default function Card({id, nome, telefone, email, descricao}) {
+export default function Card({nome, telefone, email, descricao}) {
     return (
         <div className={styles.container_card}>
             <div className={styles.card_logo}>
                 <span className="material-symbols-outlined">person</span>
             </div>
 
-            <h3>{nome}</h3>
+            <div className={styles.nome_desc}>
+                <h3>{nome}</h3>
+                <p className={styles.descricao}>{descricao}</p>
+            </div>
 
-            <p className={styles.descricao}>{descricao}</p>
+            <div className={styles.contatos}>
+                <div>
+                    <span className="material-symbols-outlined">call </span>
+                    <p className={styles.telefone}>{telefone}</p>
+                </div>
 
-            <p className={styles.telefone}>{telefone}</p>
-
-            <p className={styles.email}>{email}</p>
+                <div>
+                    <span className="material-symbols-outlined">email </span>
+                    <p className={styles.email}>{email}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,9 +4,9 @@ import { retornaMedicos } from "./data/dados.js";
 import Topo from "./components/Topo";
 import Card from "./components/Card";
 
-const medicos = await retornaMedicos();
+export default async function Home() {
+  const medicos = await retornaMedicos();
 
-export default function Home() {
   return (
     <div className={styles.page}>
       <Topo />
@@ -14,15 +14,17 @@ export default function Home() {
       <main className={styles.conteudo_principal}>
         <h2>Nossa Equipe Médica</h2>
 
-        {medicos.map((medico) => (
-          <Card 
-            key={medico.id}
-            nome={medico.nome}
-            descricao={medico.descricao}
-            email={medico.email}
-            telefone={medico.telefone}
-          />
-        ))}
+        <div className={styles.card_principal}>
+          {medicos.map((medico) => (
+            <Card
+              key={medico.id}
+              nome={medico.nome}
+              descricao={medico.descricao}
+              email={medico.email}
+              telefone={medico.telefone}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
